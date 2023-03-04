add0e553408c01590c06ec190be0947e87983dbb
valentine <valentinetuyrin@gmail.com>
2023-03-04 17:30:54 +0300

    no message

--- app/src/main/java/com/example/pifagorapp/MainActivity.kt
+++ app/src/main/java/com/example/pifagorapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.pifagorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pifagorapp.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
+    fun onClickResult(view: View) {
+        if (!isFieldEmpty()){
+            val result = getString(R.string.result_info)+ getResult()
+            binding.tvResult.text = result
+        }
+
+    }
+
+    private fun isFieldEmpty(): Boolean {
+        binding.apply {
+            if (edA.text.isNullOrEmpty()) edA.error = "zapolni pole"
+            if (edB.text.isNullOrEmpty()) edB.error = "zapolni pole"
+            return edA.text.isNullOrEmpty() && edB.text.isNullOrEmpty()
+
+        }
+    }
+
+    private fun getResult(): String {
+        val a: Double
+        val b: Double
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+
+        }
+        return sqrt((a.pow(2) + (b.pow(2)))).toString()
+
+    }
+
+
 }
